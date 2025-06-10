11f3ca081baa3172744c9ee8c99ab5acec64de51
Ansh Thakur <mailtodroidoox@gmail.com>
2025-06-10 14:04:41 +0530

    Don't forget to add the render.yaml file to the repository.

--- Lifelane - copy/server.js
+++ Lifelane - copy/server.js
@@ -3,7 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import { pool } from './server/db.js';
+import { pool, initializeDatabase } from './server/db.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
@@ -22,6 +22,9 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Initialize database
+initializeDatabase().catch(console.error);
+
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'dist')));
 
@@ -31,6 +34,12 @@ import './server/index.js';
 // Handle React routing, return all requests to React app
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 app.listen(port, () => {
